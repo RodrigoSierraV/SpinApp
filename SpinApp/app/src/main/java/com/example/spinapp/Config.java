784a189a9fd3605a4bad2d800eed315e00c050ff
RodrigoSierraV <735@holbertonschool.com>
2019-10-28 10:33:28 -0500

    Adding MVP with youtube API

---
+++ SpinApp/app/src/main/java/com/example/spinapp/Config.java
@@ -0,0 +1,8 @@
+package com.example.spinapp;
+
+public class Config {
+    private Config() {
+    }
+
+    public static final String YOUTUBE_API_KEY = "AIzaSyC00IOWVrJxRGeH2Y8tI8q7qwgzb9PPouk";
+}
